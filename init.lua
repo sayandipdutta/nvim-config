2a75752f31388fc6299f825d001f82792bda8bca
Sayandip Dutta <sayandip199309@gmail.com>
2026-05-17 06:46:12 +0000

    enable new ui

--- init.lua
+++ init.lua
@@ -1,3 +1,14 @@
+require("vim._core.ui2").enable({
+    enable = true,
+    msg = {
+        target = "cmd",
+        pager = { height = 0.5 },
+        dialog = { height = 0.5 },
+        cmd = { height = 0.5 },
+        msg = { height = 0.5, timeout = 4500 },
+    }
+})
+
 require("config.options")
 require("config.keymaps")
 require("config.autocmd")
